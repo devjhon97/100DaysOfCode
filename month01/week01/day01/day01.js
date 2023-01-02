480e2bee35beb7bca6a6eb19df98fd6f39098a77
Jhonatan Souza <120052583+devjhon97@users.noreply.github.com>
2023-01-01 23:40:20 -0300

    some details

--- month01/week01/day01/day01.js
+++ month01/week01/day01/day01.js
@@ -2,9 +2,13 @@ const readline = require("readline-sync")
 
 function ageInDays(){
     var age = readline.question("what's your age?");
-    var days = age*365
-
-    console.log(days);
+    if(age < 0){
+        console.log('insert a valid age');
+        ageInDays();
+    }else{
+        var days = age*365;
+        console.log(days);
+    }
 }
 
 ageInDays();
