87feb8c619ca9a82d4fa03df881d516aa355013f
Jhonatan Souza <120052583+devjhon97@users.noreply.github.com>
2023-01-05 13:26:29 -0300

    5th challenge in JS done

---
+++ month01/week01/day05/sockPairs.js
@@ -0,0 +1,24 @@
+function pairs(data){
+
+    if(typeof data !== 'string'){
+        return;
+    }
+
+    let str = data.split('').sort().join('');
+    let pair = 0;
+
+    for(let index = 0; index < str.length; index++){
+        if(str.charAt(index) === str.charAt(index+1)){
+            pair++;
+            index++;
+        }
+    }
+
+    return ("There are "+pair+' pairs.');
+}
+
+console.log(pairs("ABCDE")); // should have return 0
+console.log(pairs("AA")); // should have return 1
+console.log(pairs("ABABC")); // should have return 2
+console.log(pairs("CABBACCC")) // should have return 4
+console.log(pairs("ABEDEEDCCDBA")) // should have return 5
