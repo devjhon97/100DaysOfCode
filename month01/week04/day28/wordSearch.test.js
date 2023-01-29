72f9686e0f6650ad7a493c701630ea9603bcc772
Jhonatan Souza <120052583+devjhon97@users.noreply.github.com>
2023-01-29 18:07:48 -0300

    28th challenge

--- month01/week04/day28/wordSearch.test.js
+++ month01/week04/day28/wordSearch.test.js
@@ -2,6 +2,10 @@ const wordSearch = require("./wordSearch.js");
 
 describe('28th challenge, #100DaysOfCode',()=>{
     it('it should return \'eat\' and \'oath\' word',()=>{
-        expect(wordSearch([['o','a','a','n'],['e','t','a','e'],['i','h','k','r'],['i','f','l','v']],['oath','pea','eat','rain'])).toStrictEqual(['eat','oath']);
+        expect(wordSearch([['o','a','a','n'],
+                           ['e','t','a','e'],
+                           ['i','h','k','r'],
+                           ['i','f','l','v']],
+        ['oath','pea','eat','rain'])).toStrictEqual(['eat','oath']);
     })
 })
