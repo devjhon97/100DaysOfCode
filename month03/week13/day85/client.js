0c491040a8ff78ac61cd4e7a1308ed1de64f5614
devjhon97 <jhonatanvideos337@gmail.com>
2023-03-26 21:10:54 -0300

    85th challenge

--- month03/week13/day85/client.js
+++ month03/week13/day85/client.js
@@ -1,15 +1,27 @@
 const net = require('net');
 const rl = require('readline-sync');
+const client = new net.Socket();
 
 let msg = rl.question("Digite uma mensagem: ");
 
-const client = net.createConnection({port: 3000}, ()=>{
-    console.log('Conectando ao servidor!');
-    client.write(msg);
-})
+try{
+    client.connect({port: 3000}, ()=>{
+        console.log('Conectando ao servidor!');
+        client.write(msg);
+    })
+    
+    client.on('data', (data) => {
+        console.log('Conectado!\n');
+        console.log(`Mensagem devolvida: ${data.toString('utf-8')}`);
+        client.end();
+    })
 
-client.on('data', (data) => {
-    console.log('Conectado!\n');
-    console.log(`Mensagem devolvida: ${data.toString('utf-8')}`);
-    client.end();
-})
+    client.on('error', (err)=> {
+        console.log('Erro ao se conectar ao servidor!');
+        client.destroy();
+    })
+
+}catch(err) {
+    console.log('O servidor está offline!');
+    client.destroy();
+}
